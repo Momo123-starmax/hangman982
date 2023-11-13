252cc9ba09d9b96e9a43e28d59c1c7b569bcbb28
Momo123-starmax <146265366+Momo123-starmax@users.noreply.github.com>
2023-11-13 12:24:18 +0000

    Add files via upload

---
+++ hangman/milestone_3.py
@@ -0,0 +1,28 @@
+import random
+
+word_list = ['banana', 'apple', 'orange', 'strawberry', 'watermelon']
+print(random.choice(word_list))
+
+word = input('Enter a single letter:')
+
+def check_guess(guess):
+    guess = guess.lower() 
+
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+
+    else:
+        print(f"Sorry,  {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    while True:
+        guess = input("Guess a single letter:")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else: print("Invalid letter. Please, enter a single alphabetical character.")
+
+        check_guess(guess)
+
+        ask_for_input
+
+
